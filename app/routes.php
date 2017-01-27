cdb7f8ec354fa1d808e55be5c8e63c203e4e94c2
xdNoliv <oliviermairet@gmail.com>
2017-01-27 09:13:00 +0100

    MAJ Header + page d'accueil

--- app/routes.php
+++ app/routes.php
@@ -8,14 +8,6 @@ $app->get('/', function () use ($app) {
     return $app['twig']->render('index.html.twig', array('listeRestau' => $troisDerniersRestau));
 })->bind('home');
 
-// Liste des dragons
-$app->get('/dragon-liste', function () use ($app) {
-	// Requete liste dragon
-	$dragons = $app['model.dragon']->listeDragons();
-    // Envoi des résultats au template Twig
-    return $app['twig']->render('dragon/liste.html.twig', array('dragons' => $dragons));
-})->bind('dragon_liste');
-
 // Aide utilisateurs
 $app->get('/aide-utilisateurs', function () use ($app) {
 	return $app['twig']->render('aide-utilisateur.html.twig');
