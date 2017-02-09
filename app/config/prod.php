3ad67ec64e2feb6eee255f504e53d4c9c8e3edb7
Karine DANVERS <kardanvers@gmail.com>
2017-02-09 10:46:59 +0100

    categorie

--- app/config/prod.php
+++ app/config/prod.php
@@ -6,7 +6,7 @@ $app['db.options'] = array(
     'charset'  => 'utf8',
     'host'     => 'localhost',
     'port'     => '3306',
-    'dbname'   => 'bddragons',
+    'dbname'   => 'lillebio',
     'user'     => 'root',
     'password' => 'root',
 );
